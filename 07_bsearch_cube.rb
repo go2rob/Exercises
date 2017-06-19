8e6fd2b789fcac812260693ba25e3f05a7780542
ROBINSON <robmail1996@gmail.com>
2017-06-19 17:40:29 +0530

    cube root using Binary search with Big O computed

--- 07_bsearch_cube.rb
+++ 07_bsearch_cube.rb
@@ -1,32 +1,37 @@
-def search(l, r, &block)
-  m = l + ((r - l) / 2)
-  return m if(l > r)
-  case(block.call(m))
-  when -1
-    search((m + 1), r, &block)
-  when 1
-    search(l, (m - 1), &block)
-  when 0
-    m
+def search(l, r, &block)                                                                              # => O(log n)
+  m = (l + r)  / 2                                              
+  return m if(l > r)                                          # => O(n)                # => O(n) |
+  case(block.call(m))                                         #                                  |
+  when -1                                                     #                                  |
+    search((m + 1), r, &block)                                # => O(n) |                        |    # => O(n)
+  when 1                                                      #         |                        |  
+    search(l, (m - 1), &block)                                # => O(n) | # => 3*O(n)| # => O(n) |                # => O(log n) * O(n)
+  when 0                                                      #         |
+    m                                                         # => O(n) |
   end
 end
-def find_cube(n)
-  root = nil
-  result = search(0, n) { |value|
-    ((value ** 3) <=> n)
-  }
+def find_cube(n)                                                    
+  root = nil                                                  # => O(n)
+  result = search(0, n) { |value|                             
+    ((value ** 3) <=> n)          
+  }           
   if ((result ** 3) == n)
-    return result
-  else
+    return result                                             # => O(n)           
+  else                                                        #                     | # => O(log n)   # => O(n) + O(log n)
   16.times { |i|
-    result = search(((result * 10) + 0), ((result * 10) + 9)) { |value|
+    result = search(((result * 10) + 0), ((result * 10) + 9)) { |value| # => O(log n)   
     (value ** 3) <=> (n * (1000 ** (i + 1)))
   }
-  root = result / (10.0 ** (i + 1))
-}
-root
+  root = result / (10.0 ** (i + 1))                           # => O(n)
+}                                                             #                     | # => O(n)  
+root                                                          # => O(n)
 end
 end
-x = 4930355
+x = 148876                                                    # => O(n)
 puts find_cube(x)
 puts Math.cbrt(x)
+
+# summary     (O(log n) * O(n)) + O(n) + O(log n) + O(n)
+#             O(n * log n) + O(n) + O (log n)
+
+# => O (n*log n)
