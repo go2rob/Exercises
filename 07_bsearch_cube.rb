46236a1b902736d7c57497b0111063eda10fe355
ROBINSON <robmail1996@gmail.com>
2017-06-19 13:28:55 +0530

    fixed the logic

--- 07_bsearch_cube.rb
+++ 07_bsearch_cube.rb
@@ -1,10 +1,6 @@
-$counter = 0
 def search(l, r, &block)
   m = l + ((r - l) / 2)
-  puts "m:#{m},l:#{l},r:#{r}"
-  return nil if(l > r)
-  $counter += 1
-  return "NEVERENDING" if($counter > 20)
+  return m if(l > r)
   case(block.call(m))
   when -1
     search((m + 1), r, &block)
@@ -14,44 +10,23 @@ def search(l, r, &block)
     m
   end
 end
-
 def find_cube(n)
-  # result = search(1, n) { |value|
-  #   puts "compare: #{((value ** 3) <=> n)}"
-  #   ((value ** 3) <=> n)
-  # }
-  result = 4
-  # if(result.nil?)
-  #   result = search(1, n) { |value|
-  #     puts "value: #{value}"
-  #     cube = (value ** 3)
-  #     if(cube < n)
-  #       (((value + 1) ** 3) > n) ? 0 : -1
-  #     else
-  #       1
-  #     end
-  #   }
-  # else
-  #   return result
-  # end
-  
-  $counter = 0
-  puts "whole: #{result}"
-  10.times { |i|
-    dp = search(0, 9) { |value|
-      frag = result + (value * (10 ** (i + 1)))
-      if(frag == n)
-        0
-      elsif(frag < n)
-        nfrag = result + ((value + 1) * (10 ** (i + 1)))
-        (nfrag > n) ? 0 : -1
-      else
-        1
-      end
-    }
-    result =  result + (dp * (10 ** (i + 1)))
+  root = nil
+  result = search(0, n) { |value|
+    ((value ** 3) <=> n)
   }
-  return result
+  if ((result ** 3) == n)
+    return result
+  else
+  16.times { |i|
+    result = search(((result * 10) + 0), ((result * 10) + 9)) { |value|
+    (value ** 3) <=> (n * (1000 ** (i + 1)))
+  }
+  root = result / (10.0 ** (i + 1))
+}
+root
 end
-require 'byebug'
-puts find_cube(124)
+end
+x = 4930355
+puts find_cube(x)
+puts Math.cbrt(x)
